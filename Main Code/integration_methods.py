f28471a2c85209270228d34d8d5cde6680da6b9f
LAB <idankes1@gmail.com>
2021-09-14 13:32:18 +0300

    refactoring

---
+++ Main Code/integration_methods.py
@@ -0,0 +1,80 @@
+import re
+import fabio
+import pyFAI
+from silx.gui.qt import QMessageBox, QFont
+
+def full_integration(self, image, mask, poni, bins, minradius, maxradius, q_choice, datadict, nxs, nxs_file_dict):
+    if not nxs:
+        imagefolder = self.imagepath
+        imagepath = imagefolder + '/' + image
+        img = fabio.open(imagepath)
+        filename = image.split('.')[0]
+        img_array = img.data
+    if nxs:
+        imagefolder = self.imagepath
+        image_name = image.split('.')[0] + '.nxs'
+        image_data = nxs_file_dict[image_name][image]
+        img_array = image_data
+        filename = image
+    ai = pyFAI.load(poni)
+    res = ai.integrate1d_ng(img_array,
+                            bins,
+                            mask=mask,
+                            unit=q_choice,
+                            filename="{}/{}.dat".format(imagefolder, filename),
+                            error_model='poisson',
+                            radial_range=(minradius, maxradius))
+    datadict[filename] = res
+
+
+def send_to_integration(self, imagelist):
+    bins, minradius, maxradius, poni, mask, q_choice, nxs_file_dict, datadict, loadedlist,plot=self.getIntegrationParams()
+    tw = self.tw
+    loadeditemsTextList = [str(loadedlist.item(i).text()) for i in range(loadedlist.count())]
+    if len(imagelist) == 0:
+        msg = QMessageBox()
+        msg.setWindowTitle("Error")
+        msg.setText("Please Select an Image to Integrate")
+        x = msg.exec_()
+    else:
+        length=len(imagelist)
+        i=1
+        for image in imagelist:
+            pvalue=int((i/length)*100)
+            self.progressbar.setValue(pvalue)
+            self.progressbar.setFont(QFont('Segoe UI',9))
+            if image not in loadeditemsTextList:
+                if (image.endswith('.tiff') or image.endswith('.tif')):
+                    full_integration(self,image=image, poni=poni, mask=mask.data, bins=bins, minradius=minradius,
+                                          maxradius=maxradius, q_choice=q_choice, datadict=datadict, nxs=False,
+                                          nxs_file_dict=nxs_file_dict)
+
+                    filename = image.split('.')[0]
+                    res = datadict[filename]
+                    plot.addCurve(x=res.radial, y=res.intensity, yerror=res.sigma, legend='{}'.format(filename),
+                                  linewidth=2)
+                    loadedlist.addItem(image)
+                regexp = re.compile(r'(?:nxs - image ).*$')
+                if regexp.search(image):
+                    full_integration(self,image=image, poni=poni, mask=mask.data, bins=bins, minradius=minradius,
+                                          maxradius=maxradius, q_choice=q_choice, datadict=datadict, nxs=True,
+                                          nxs_file_dict=nxs_file_dict)
+                    res = datadict[image]
+                    plot.addCurve(x=res.radial, y=res.intensity, yerror=res.sigma, legend='{}'.format(image),
+                                  linewidth=2)
+                    loadedlist.addItem(image)
+                if image.endswith('.nxs'):
+                    None
+
+            else:
+                if (image.endswith('.tiff') or image.endswith('.tif')):
+                    filename = image.split('.')[0]
+                    res = datadict[filename]
+                    plot.addCurve(x=res.radial, y=res.intensity, yerror=res.sigma, legend='{}'.format(filename),
+                                  linewidth=2)
+                regexp = re.compile(r'(?:nxs - image ).*$')
+                if regexp.search(image):
+                    res = datadict[image]
+                    plot.addCurve(x=res.radial, y=res.intensity, yerror=res.sigma, legend='{}'.format(image),
+                                  linewidth=2)
+            i+=1
--- Main Code/main.py
+++ Main Code/main.py
@@ -3,18 +3,17 @@ import re
 from silx.gui import qt
 from skimage import io
 from silx.gui.plot.PlotWindow import PlotWindow
-import pyFAI, fabio
+import fabio
 import subprocess
-from PIL import Image, ImageOps
-import PIL
+from PIL import Image
 from silx.gui.plot import tools
 from PyQt5 import QtWidgets
 from silx.gui.widgets.BoxLayoutDockWidget import BoxLayoutDockWidget
-from PyQt5.QtWidgets import QMessageBox
 from docking_bars import MyCurveLegendsWidget
 from open_methods import open_directory,open_poni,open_mask,open_nxs
 from plotting_methods import image_plot,curve_plot,plot_mul_curves,subtractcurves
 from saving_methods import save_csv
+from integration_methods import full_integration,send_to_integration
 
 
 class MyPlotWindow(qt.QMainWindow):
@@ -219,91 +218,16 @@ class MyPlotWindow(qt.QMainWindow):
         plot = self.getPlotWidget()
         plot.getDefaultColormap().setName('viridis')
         im = Image.open('files/saxsii.jpeg')
-        im=im.rotate(180, PIL.Image.NEAREST, expand = 1)
-        im_mirror = PIL.ImageOps.mirror(im)
-        plot.addImage(im_mirror)
+        im=numpy.flip(im,0)
+        plot.addImage(im)
 
     def InitiateCalibration(self):
         subprocess.run(["pyFAI-calib2"])
 
-    def full_integration(self,image,mask,poni,bins,minradius,maxradius,q_choice,datadict,nxs,nxs_file_dict):
-        if not nxs:
-            imagefolder=self.imagepath
-            imagepath=imagefolder+'/'+image
-            img = fabio.open(imagepath)
-            filename = image.split('.')[0]
-            img_array = img.data
-        if nxs:
-            imagefolder = self.imagepath
-            image_name = image.split('.')[0]+'.nxs'
-            image_data = nxs_file_dict[image_name][image]
-            img_array=image_data
-            filename=image
-        ai = pyFAI.load(poni)
-        res = ai.integrate1d_ng(img_array,
-                                bins,
-                                mask=mask,
-                                unit=q_choice,
-                                filename="{}/{}.dat".format(imagefolder,filename),
-                                error_model='poisson',
-                                radial_range=(minradius, maxradius))
-        datadict[filename] = res
-
-    def integrate(self,imagelist):
-        bins, minradius, maxradius, poni, mask, q_choice, nxs_file_dict, datadict, loadedlist,plot=self.getIntegrationParams()
-        tw = self.tw
-        loadeditemsTextList = [str(loadedlist.item(i).text()) for i in range(loadedlist.count())]
-        if len(imagelist) == 0:
-            msg = QMessageBox()
-            msg.setWindowTitle("Error")
-            msg.setText("Please Select an Image to Integrate")
-            x = msg.exec_()
-        else:
-            length=len(imagelist)
-            i=1
-            for image in imagelist:
-                pvalue=int((i/length)*100)
-                self.progressbar.setValue(pvalue)
-                self.progressbar.setFont(qt.QFont('Segoe UI',9))
-                if image not in loadeditemsTextList:
-                    if (image.endswith('.tiff') or image.endswith('.tif')):
-                        self.full_integration(image=image, poni=poni, mask=mask.data, bins=bins, minradius=minradius,
-                                              maxradius=maxradius, q_choice=q_choice, datadict=datadict, nxs=False,
-                                              nxs_file_dict=nxs_file_dict)
-
-                        filename = image.split('.')[0]
-                        res = datadict[filename]
-                        plot.addCurve(x=res.radial, y=res.intensity, yerror=res.sigma, legend='{}'.format(filename),
-                                      linewidth=2)
-                        loadedlist.addItem(image)
-                    regexp = re.compile(r'(?:nxs - image ).*$')
-                    if regexp.search(image):
-                        self.full_integration(image=image, poni=poni, mask=mask.data, bins=bins, minradius=minradius,
-                                              maxradius=maxradius, q_choice=q_choice, datadict=datadict, nxs=True,
-                                              nxs_file_dict=nxs_file_dict)
-                        res = datadict[image]
-                        plot.addCurve(x=res.radial, y=res.intensity, yerror=res.sigma, legend='{}'.format(image),
-                                      linewidth=2)
-                        loadedlist.addItem(image)
-                    if image.endswith('.nxs'):
-                        None
-
-                else:
-                    if (image.endswith('.tiff') or image.endswith('.tif')):
-                        filename = image.split('.')[0]
-                        res = datadict[filename]
-                        plot.addCurve(x=res.radial, y=res.intensity, yerror=res.sigma, legend='{}'.format(filename),
-                                      linewidth=2)
-                    regexp = re.compile(r'(?:nxs - image ).*$')
-                    if regexp.search(image):
-                        res = datadict[image]
-                        plot.addCurve(x=res.radial, y=res.intensity, yerror=res.sigma, legend='{}'.format(image),
-                                      linewidth=2)
-                i+=1
     def Integrate_selected(self):
         tw=self.tw
         imagelist=[item.text(0) for item in tw.selectedItems()]
-        self.integrate((imagelist))
+        send_to_integration(self,imagelist)
 
     def Integrate_all(self):
         def get_subtree_nodes(tree_widget_item):
@@ -325,7 +249,7 @@ class MyPlotWindow(qt.QMainWindow):
         tw=self.tw
         imagelist=get_all_items(tw)
         imagelist_names=[image.text(0) for image in imagelist]
-        self.integrate(imagelist_names)
+        send_to_integration(self,imagelist_names)
 
     def open_directory_wrap(self):
         open_directory(self)
